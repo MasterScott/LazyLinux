ec056a48209297b4a13b5d70bb1210ca1c539aac
Eno12345 <Eno123456@users.noreply.github.com>
2017-08-09 20:03:30 -0600

    Update LazyLinux.py

--- LazyLinux.py
+++ LazyLinux.py
@@ -1,2 +1,9 @@
-import os
+from os import system
 from time import sleep
+def choose:
+   print "Select An Option:"
+   print "1. Spoof/Spam Email"
+option = input(">")
+while option != 1:
+  print"INVALID OPTION"
+  choose()
