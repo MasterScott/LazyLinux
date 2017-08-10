6adda3ecf69e6bd4c30c62e9afa1a046c46c51d3
Eno12345 <Eno123456@users.noreply.github.com>
2017-08-09 20:32:19 -0600

    Update LazyLinux.py

--- LazyLinux.py
+++ LazyLinux.py
@@ -9,4 +9,9 @@ while option != 1:
   choose()
  if option == 1:
    print "You need a STMP server. \n If you don't have one, I suggest STMP2GO.com\n\n\n"
+   print ("If you want to set any of these variables as default, edit the code in "LazyLinux.py"
+   sleep(3)
+   stmpserver = raw_input("stmp server(example:stmp.stmp2go.com): ")
+   stmppass = raw_input("stmp server password: ")
+   to 
    
